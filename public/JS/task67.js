b36578c6c54c944b30b6d5626e0cfc7481c2a23a
Luna <jasonpowben@gmail.com>
2025-10-16 12:14:30 +0100

    week 5

---
+++ public/JS/task67.js
@@ -0,0 +1,9 @@
+function totup(myForm) {
+  let total = 0;
+  for (let i = 0; i < myForm.elements.entry.length; i++) {
+    console.log("no value", myForm.elements.entry[i].value);
+    total += parseFloat(myForm.elements.entry[i].value);
+  }
+  total = total * (1 - myForm.elements.d1.value / 100);
+  document.getElementById("totalled").innerHTML = total.toFixed(2);
+}
